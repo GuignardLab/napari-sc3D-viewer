b2104389e129400031b8cadbe980ee47fc902dc8
Léo Guignard <leo.guignard@univ-amu.fr>
2022-05-23 14:51:16 +0200

    test

--- src/napari_sc3d_viewer/_tests/test_widget.py
+++ src/napari_sc3d_viewer/_tests/test_widget.py
@@ -1,6 +1,15 @@
-import numpy as np
 from napari_sc3d_viewer import Startsc3D
 
+to_test = ['Select tissues',
+           'Show gene/metric',
+           'Show gene',
+           'Color according to cell types',
+           'Adjust contrast',
+           'Threshold cells',
+           'Show gene on Umap',
+           'Save',
+           'Tight Layout',
+           'Compute and show surface']
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
@@ -14,9 +23,18 @@ def test_example_q_widget(make_napari_viewer, capsys):
     # call our widget method
     my_widget.h5ad_file.value = 'test_data/data_test.h5ad'
     my_widget.json_file.value = 'test_data/corresptissues.json'
-    my_widget._on_click()
+    v, t1, t2 = my_widget._on_click()
+
+    to_treat = t1.children() + t2.children()
+    while 0<len(to_treat):
+        current = to_treat.pop()
+        if hasattr(current, 'click'):
+            if current.text() in to_test:
+                current.click()
+            # current.click()
+        to_treat += current.children()
 
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
-    print(captured)
+    print(captured.out)
     # assert captured.out == "napari has 1 layers\n"
