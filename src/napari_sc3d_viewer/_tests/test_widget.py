b042f997495334d00854e7cad88917e76b8f309c
Léo Guignard <leo.guignard@univ-amu.fr>
2022-07-11 18:15:55 +0200

    passing now?

--- src/napari_sc3d_viewer/_tests/test_widget.py
+++ src/napari_sc3d_viewer/_tests/test_widget.py
@@ -19,7 +19,7 @@ def test_example_q_widget(make_napari_viewer, capsys):
     # call our widget method
     my_widget.h5ad_file.value = 'test_data/data_test.h5ad'
     my_widget.json_file.value = 'test_data/wrong_format.json'
-    my_widget.sample_list_value.value = ''
+    my_widget.sample_list.value = ''
     displayed_embryo = my_widget._on_click_load()
     my_widget.json_file.value = 'test_data/corresptissues.json'
     displayed_embryo = my_widget._on_click_load()
