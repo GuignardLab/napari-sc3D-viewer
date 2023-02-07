54c9cf3aba6161c09587bd4520868da74142ca26
Léo Guignard <leo.guignard@univ-amu.fr>
2023-02-07 17:43:51 +0100

    remove useless imports

--- src/napari_sc3d_viewer/_display_embryo.py
+++ src/napari_sc3d_viewer/_display_embryo.py
@@ -3,7 +3,6 @@ This file is subject to the terms and conditions defined in
 file 'LICENCE', which is part of this source code package.
 Author: Leo Guignard (leo.guignard...@AT@...univ-amu.fr)
 """
-import json
 from qtpy.QtWidgets import QTabWidget, QVBoxLayout, QWidget
 from magicgui import widgets
 from ._umap_selection import UmapSelection
