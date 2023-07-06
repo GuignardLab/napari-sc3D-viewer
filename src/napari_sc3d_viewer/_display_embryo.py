de977b3c89f75967ed3ed9ff9cdf1d5ad0a1a3d1
Léo Guignard <leo.guignard@univ-amu.fr>
2023-07-06 11:08:46 +0200

    linting

--- src/napari_sc3d_viewer/_display_embryo.py
+++ src/napari_sc3d_viewer/_display_embryo.py
@@ -102,9 +102,8 @@ class DisplayEmbryo:
                     m, M = 0, 1
                 else:
                     m, M = points.face_contrast_limits
-                if (
-                    points.face_colormap.name in plt.colormaps()
-                    or isinstance(points.face_colormap, Colormap)
+                if points.face_colormap.name in plt.colormaps() or isinstance(
+                    points.face_colormap, Colormap
                 ):
                     if points.face_colormap.name in plt.colormaps():
                         cmap = points.face_colormap.name
@@ -447,23 +446,14 @@ class DisplayEmbryo:
         else:
             init_value = self.grey.value
             cmap_mpl = {
-                "red": [
-                    [0., init_value, init_value],
-                    [1., 0., 0.]
-                ],
-                "blue": [
-                    [0., init_value, init_value],
-                    [1., 0., 0.]
-                ],
-                "green": [
-                    [0., init_value, init_value],
-                    [1., 0., 0.]
-                ],
+                "red": [[0.0, init_value, init_value], [1.0, 0.0, 0.0]],
+                "blue": [[0.0, init_value, init_value], [1.0, 0.0, 0.0]],
+                "green": [[0.0, init_value, init_value], [1.0, 0.0, 0.0]],
             }
             cmap_mpl[self.manual_color.value.lower()] = [
-                    [0., init_value, init_value],
-                    [1., 1., 1.]
-                ]
+                [0.0, init_value, init_value],
+                [1.0, 1.0, 1.0],
+            ]
             if self.manual_color.value == "Red":
                 color = 0
             elif self.manual_color.value == "Green":
@@ -1055,7 +1045,7 @@ class DisplayEmbryo:
             properties=properties,
             metadata={"gene": None, "2genes": None},
             shown=shown,
-            size=15
+            size=15,
         )
 
         self.all_tissues = [
